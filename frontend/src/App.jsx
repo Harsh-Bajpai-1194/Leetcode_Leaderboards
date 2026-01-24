3a3deaef82b45428be2d4702b074434cb5ef33d9
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-24 22:50:49 +0530

    Add routing for Leaderboard and AdminPanel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Leaderboard from './Leaderboard';
+import AdminPanel from './AdminPanel';
 
 function App() {
   return (
-    <div className="App">
-      <Leaderboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Main Page */}
+          <Route path="/" element={<Leaderboard />} />
+          
+          {/* Hidden Admin Page */}
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
