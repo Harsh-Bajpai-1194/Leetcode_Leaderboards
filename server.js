4755d1c98d04dbc3e47eed631b5a4dd0fbb90227
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-04-08 23:10:28 +0530

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -196,6 +196,6 @@ app.post('/api/trigger-update', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
